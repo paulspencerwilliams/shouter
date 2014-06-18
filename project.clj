6d96f3dac6f263901b1506ed72915026cc8535c7
Paul Williams <paul@paulswilliams.me.uk>
2014-06-18 13:10:55 +0100

    updated project references

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject shouter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject shouter "0.0.1"
+  :description "Shouter app"
+  :url "http://github.com/abedra/shouter"
+  :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/java.jdbc "0.3.2"]
+                 [postgresql "9.1-901.jdbc4"]])
